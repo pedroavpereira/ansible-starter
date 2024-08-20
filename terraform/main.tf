a4f8fbdf011a54fbeb175c40a90a7771855ab3c9
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-20 11:53:28 +0100

    Reduce instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_instance" "http_server" {
-  count                  = 2
+  count                  = 1
   ami                    = "INSERT AMI HERE"
   key_name               = "default-ec2"
   instance_type          = "t2.micro"
